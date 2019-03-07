29cb8d3b92aadffd6e9fe742924ef2e89fa460fe
artemisarcher <43240492+artemisarcher@users.noreply.github.com>
2019-03-07 08:27:16 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+$(".salem").mouseover(function(){
+    $(".cat").fadeToggle();   
+});
+
+$(".weird").click(function(){
+    $(".sis").slideToggle(); 
+});
+
+$(".ww").dblclick(function(){
+    $(".teach").toggle(); 
+});
+
+$(".one").mouseover(function(){
+    $(".dl").toggle();  
+});
+
+$(".two").mouseover(function(){
+   $(".spell").show();
+});
